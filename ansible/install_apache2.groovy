0cdcb76b15d3aa992a2c18d614fe29bece0c36b8
loboff1 <loboff-good@mail.ru>
2021-12-08 22:17:08 +0300

    edit pipeline

--- ansible/install_apache2.groovy
+++ ansible/install_apache2.groovy
@@ -2,16 +2,26 @@ pipeline {
     agent any
 
     stages {
-        stage('Checkout') {
+    
+    stage('Clean workspace') {
+            steps {
+                CleanWs()
+            }
+        stage('Check Git') {
             steps {
                 git branch: 'main', url: "git@github.com:loboff1/ansible.git", credentialsId: 'github_key'
             }
         }
-        stage('Deploy') {
+        stage('Deploy Apache2') {
             steps {
                 withCredentials([file(credentialsId: 'vault_token', variable: 'VAULT_TOKEN')]) {
         ansiblePlaybook playbook: 'ansible/playbook-roles.yaml', inventory: 'ansible/hosts', sudoUser: null, extras: "--vault-password-file ${VAULT_TOKEN}"
         }
+        
+        stage('Test') {
+            steps {
+                sh 'apache2 --version > apache2.txt'
+            }
             }
         }
     }
